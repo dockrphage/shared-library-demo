2f9ef5dd6ac8a1387ea1d7bae75dc46344bb4519
Ville <dockrphage@gmail.com>
2026-04-20 15:58:36 +0100

    Enhance Utils class with logging functionality

--- src/org/cwvj/Utils.groovy
+++ src/org/cwvj/Utils.groovy
@@ -2,17 +2,21 @@ package org.cwvj
 
 class Utils {
 
-    def steps   // injected pipeline steps
+    def steps
+    Logger logger
 
     Utils(steps) {
         this.steps = steps
+        this.logger = new Logger(steps, "UTILS")
     }
 
     def shout(String message) {
+        logger.info("Shouting message")
         steps.echo "UTILS: ${message.toUpperCase()}"
     }
 
     def repeat(String message, int times) {
+        logger.info("Repeating message ${times} times")
         (1..times).each {
             steps.echo "REPEAT ${it}: ${message}"
         }
